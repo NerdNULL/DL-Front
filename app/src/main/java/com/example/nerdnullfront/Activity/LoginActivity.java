7c05c4fe3d0b06f5aca7e3e4fe6bf2fc38053e48
Lee_seon_min <tjsalsrns96@naver.com>
2021-09-07 18:54:39 +0900

    LoginActivity-2

---
+++ app/src/main/java/com/example/nerdnullfront/Activity/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.nerdnullfront.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nerdnullfront.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private ImageView logoImage;
+    private Button loginButton;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setID();
+        setEvents();
+    }
+    public void setID(){
+        logoImage=findViewById(R.id.logoImage_LoginActivity);
+        loginButton=findViewById(R.id.loginButton_LoginActivity);
+    }
+    public void setEvents(){
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //func
+            }
+        });
+    }
+}
