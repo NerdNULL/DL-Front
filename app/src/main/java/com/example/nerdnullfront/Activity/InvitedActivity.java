8e5cb0cf7d05df497102633037c973c215b464e1
MMD <ghkd2676@naver.com>
2021-09-19 16:46:20 +0900

    resource update

---
+++ app/src/main/java/com/example/nerdnullfront/Activity/InvitedActivity.java
@@ -0,0 +1,37 @@
+package com.example.nerdnullfront.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nerdnullfront.R;
+
+public class InvitedActivity extends AppCompatActivity {
+    Button button_ok = (Button) findViewById(R.id.button_ok); //수락버튼
+    Button button_reject = (Button) findViewById(R.id.button_reject); //거절버튼
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invited);
+        button_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(InvitedActivity.this,"수락되었습니다.",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button_reject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(InvitedActivity.this,"거절되었습니다.",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
